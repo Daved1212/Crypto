b0dc7c558ffee7b71473928126fbf2b52691e766
David <daved3128@aol.com>
2022-01-24 16:51:02 -0500

    table on page

--- src/Components/CurrencyConverter.js
+++ src/Components/CurrencyConverter.js
@@ -1,27 +1,59 @@
-import { table } from "console"
 import ExchangeRate from "./ExchangeRate"
 
 const CurrencyConverter = () => {
+  const currencies = ['BTC', 'ETH', 'USD', 'XRP', 'LTC', 'ADA']
+
+
   return (
+    <div className="input-box">
       <div className="currency-converter">
        <h2>CurrencyConverter</h2>
 
-      <table>
-        <body>
-          <tr>
-            <td>Primary Currency:</td>
-            <td>
-              <input 
-                type="number"
-                name="currency-amount-1"
-                value={""}
-              />
-            </td>
-          </tr>
-        </body>
-      </table>
+        <table>
+          <body>
+            <tr>
+              <td>Primary Currency:</td>
+              <td>
+                <input 
+                  type="number"
+                  name="currency-amount-1"
+                  value={""}
+                />
+              </td>
+              <td>
+                <select 
+                  value={""}
+                  name="currency-option-1"
+                  className="currency-options"
+                  >
+                  <option></option>  
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <td>Secondary Currency:</td>
+              <td>
+                <input 
+                  type="number"
+                  name="currency-amount-2"
+                  value={""}
+                />
+              </td>
+              <td>
+                <select 
+                  value={""}
+                  name="currency-option-2"
+                  className="currency-options"
+                  >
+                  <option></option>  
+                </select>
+              </td>
+            </tr>
+          </body>
+        </table>
 
         <ExchangeRate />
+      </div>
       </div>
   )
 }
